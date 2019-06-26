2f81bc26bc6bec0bd958cc2cbf433bb99bc3327c
Matthew Young <matthewy95@live.com>
2019-06-26 16:07:50 -0500

    updates to heroku

--- server.ts
+++ server.ts
@@ -4,12 +4,31 @@ const path = require('path');
 
 const app = express();
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/Demo'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/Demo/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/Demo/index.html'));
+})
+
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 // Start the app by listening on the default Heroku port
