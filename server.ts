fc75cc97a95ba0b3451443520f1d4e0a269bfc6d
Matthew Young <matthewy95@live.com>
2019-06-26 17:48:59 -0500

    updates to heroku

--- server.ts
+++ server.ts
@@ -15,5 +15,20 @@ app.get('/*', function (req, res) {
 
 app.use('/api/testdata', test);
 
+// allow access from client server
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin','*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the   requests sent
+    // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+    // Pass to next layer of middleware
+    next();
+  });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
